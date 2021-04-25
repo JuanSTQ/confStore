71f1ed9ae08e17f1436ebba9410486424f0b6848
juanstq <juanstq20@gmail.com>
2021-04-25 18:56:20 -0500

    Info payment

--- src/hooks/useGoogleAddress.js
+++ src/hooks/useGoogleAddress.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 
 const useGoogleAddress = address=>{
   const [map, setMap] = useState({});
-  const API = `https://maps.googleapis.com/maps/api/geocode/json?address=${address}&key=${process.env.APIKEYGOOGLE}`
+  const API = `https://maps.googleapis.com/maps/api/geocode/json?address=${address}&key=AIzaSyBBXNLc81kppB_WOPhXdW4B7WysjOk3spk`
   useEffect(async()=>{
     const response = await axios(API) //Axios nos retorna un objeto no hace falta data=>JSON
     setMap(response.data.results[0].geometry.location)
